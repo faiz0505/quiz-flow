40bb01ce6259e0e54f3a8bd5eb59e78381fcba75
Faiz Al <faizali786313@gmail.com>
2024-01-28 16:31:49 +0530

    npm run build

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-  const sessionTokenCookie =
+  const isAuthenticate =
     request.cookies.get("next-auth.session-token") ||
     request.cookies.get("__Secure-next-auth.session-token");
   const path = request.nextUrl.pathname;
@@ -27,6 +27,5 @@ export async function middleware(request) {
 }
 
 export const config = {
-  // Use an array of objects for more specific matching
   matcher: ["/profile", "/signin", "/signup", "/quiz-start/:path*"],
 };
