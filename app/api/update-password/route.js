712cc12c92892edabb66ebd3397acbb59de071e1
Faiz Al <faizali786313@gmail.com>
2024-02-07 11:14:46 +0530

    forgot page updated

--- app/api/update-password/route.js
+++ app/api/update-password/route.js
@@ -1,17 +1,19 @@
 import { NextResponse } from "next/server";
 import { dbConnection } from "@/app/lib/db/connection";
 import { User } from "@/app/lib/db/model";
+import bcrypt from "bcrypt";
+
 export async function PATCH(req) {
   const body = await req.json();
   try {
     const { email, password } = body;
     await dbConnection();
-
+    const encryptedPassword = await bcrypt.hash(password, 5);
     await User.updateOne(
       { email: email },
       {
         $set: {
-          password: password,
+          password: encryptedPassword,
         },
       }
     );
